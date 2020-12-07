2595a4d35530b314d65fc1379f727e8c0c2f1a78
Sebastian_Esser <sebastian.esser@tum.de>
2020-12-07 08:00:25 +0100

    getting datatypes

--- ExpressBinding/src/OpenInfraPlatform/ExpressBinding/Generator/GeneratorGrGenMeta.h
+++ ExpressBinding/src/OpenInfraPlatform/ExpressBinding/Generator/GeneratorGrGenMeta.h
@@ -1,6 +1,6 @@
 /*
-	Sebastian Esser, M.Sc.
-	TUM Chair of computational Modeling and Simulation
+    Sebastian Esser, M.Sc.
+    TUM Chair of computational Modeling and Simulation
 */
 
 #pragma once
@@ -8,61 +8,48 @@
 #include "OpenInfraPlatform/ExpressBinding/Generator/Generator.h"
 #include "OpenInfraPlatform/ExpressBinding/Meta/Schema.h"
 #include "OpenInfraPlatform/ExpressBinding/namespace.h"
+#include <cassert>
 #include <iostream>
 #include <map>
-#include <cassert>
 
 OIP_NAMESPACE_OPENINFRAPLATFORM_EXPRESSBINDING_BEGIN
 
 class GeneratorGrGenMeta : public Generator {
 public:
 	// Constructor
-	GeneratorGrGenMeta() {};
+	GeneratorGrGenMeta(){};
 	// Destructor
-	virtual ~GeneratorGrGenMeta() {};
+	virtual ~GeneratorGrGenMeta(){};
 
-	// 
+	//
 	void generate(std::ostream& out, OpenInfraPlatform::ExpressBinding::Schema& schema) {
-		for (int i = 0; i < schema.getEntityCount(); i++)
-		{
-			auto& entity = schema.getEntityByIndex(i);			
+		for (int i = 0; i < schema.getEntityCount(); i++) {
+			auto& entity = schema.getEntityByIndex(i);
 			std::cout << i << "\t" << entity.getName() << "\n";
 
 			auto counter = entity.getAttributeCount();
 
-			for (int j = 0; j < counter; j++)
-			{
+			for (int j = 0; j < counter; j++) {
 				auto attrName = entity.getAttribute(j).getName();
+				std::cout << "\t" << attrName << " ";
+				
 				auto attrDataType = entity.getAttribute(j).type;
 
-				/*
-				 *for (auto attr : attributes) {
-					if (attr.type->getType() == eEntityAttributeParameterType::TypeNamed) {
-						if (schema.hasEntity(attr.type->toString())) {
-							entityAttributes.insert(attr.type->toString());
-						}
-						if (schema.hasType(attr.type->toString())) {
-							typeAttributes.insert(attr.type->toString());
-						}
-					} else if (attr.type->getType() == eEntityAttributeParameterType::eGeneralizedType) {
-						auto elementType = attr.type;
-						while (elementType->getType() == eEntityAttributeParameterType::eGeneralizedType) {
-							elementType = std::static_pointer_cast<EntityAttributeGeneralizedType>(elementType)->elementType;
-						}
-						if (schema.hasEntity(elementType->toString())) {
-							entityAttributes.insert(elementType->toString());
-						}
-						if (schema.hasType(elementType->toString())) {
-							typeAttributes.insert(elementType->toString());
-						}
-					}
-				}*/
+				switch (attrDataType->getType()) {
+				case eEntityAttributeParamterType::Simple: {
+					std::cout << "simple Datatype \n";
+				} break;
 
-				
-				std::cout << "\t" << attrName << ": " << attrDataType << "\n";				
-			}
-			
-			
+					case eEntityAttributeParamterType::TypeNamed: {
+					std::cout << "typeNamed Datatype \n";
+				} break;
+
+					case eEntityAttributeParamterType::eGeneralizedType: {
+					std::cout << "generalized Type \n";
+				} break;
+					
+				}				
+ 			}
 		}
 	}
 
